c985167fbae478844155856870cdc63bde8b32dc
Shane Osbourne <sosbourne@duckduckgo.com>
2023-07-19 11:08:24 +0100

    windows polyfills?

--- integration-test/playwright/page-objects/duckplayer-overlays.js
+++ integration-test/playwright/page-objects/duckplayer-overlays.js
@@ -7,6 +7,7 @@ import {
 } from '@duckduckgo/messaging/lib/test-utils.mjs'
 import { expect } from '@playwright/test'
 import { perPlatform } from '../type-helpers.mjs'
+import { windowsGlobalPolyfills } from '../shared.mjs'
 
 // Every possible combination of UserValues
 const userValues = {
@@ -348,29 +349,10 @@ export class DuckplayerOverlays {
                  * In CI, the global objects such as USB might not be installed on the
                  * version of chromium running there.
                  */
-                await this.page.addInitScript(() => {
-                    // @ts-expect-error - testing
-                    if (typeof Bluetooth === 'undefined') {
-                        globalThis.Bluetooth = {}
-                        globalThis.Bluetooth.prototype = { requestDevice: async () => { /* noop */ } }
-                    }
-                    // @ts-expect-error - testing
-                    if (typeof USB === 'undefined') {
-                        globalThis.USB = {}
-                        globalThis.USB.prototype = { requestDevice: async () => { /* noop */ } }
-                    }
-
-                    // @ts-expect-error - testing
-                    if (typeof Serial === 'undefined') {
-                        globalThis.Serial = {}
-                        globalThis.Serial.prototype = { requestPort: async () => { /* noop */ } }
-                    }
-                    // @ts-expect-error - testing
-                    if (typeof HID === 'undefined') {
-                        globalThis.HID = {}
-                        globalThis.HID.prototype = { requestDevice: async () => { /* noop */ } }
-                    }
-                })
+                await this.page.addInitScript(windowsGlobalPolyfills)
+            },
+            'apple-isolated': async () => {
+                // noop
             }
         })
 
