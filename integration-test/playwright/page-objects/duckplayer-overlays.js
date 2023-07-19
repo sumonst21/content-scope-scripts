870d5035af90b95c55749ad62ea088f84a649cc1
Shane Osbourne <sosbourne@duckduckgo.com>
2023-07-19 11:01:35 +0100

    windows polyfills?

--- integration-test/playwright/page-objects/duckplayer-overlays.js
+++ integration-test/playwright/page-objects/duckplayer-overlays.js
@@ -342,6 +342,38 @@ export class DuckplayerOverlays {
     async setup (params) {
         const { config } = params
 
+        await this.build.switch({
+            windows: async () => {
+                /**
+                 * In CI, the global objects such as USB might not be installed on the
+                 * version of chromium running there.
+                 */
+                await this.page.addInitScript(() => {
+                    // @ts-expect-error - testing
+                    if (typeof Bluetooth === 'undefined') {
+                        globalThis.Bluetooth = {}
+                        globalThis.Bluetooth.prototype = { requestDevice: async () => { /* noop */ } }
+                    }
+                    // @ts-expect-error - testing
+                    if (typeof USB === 'undefined') {
+                        globalThis.USB = {}
+                        globalThis.USB.prototype = { requestDevice: async () => { /* noop */ } }
+                    }
+
+                    // @ts-expect-error - testing
+                    if (typeof Serial === 'undefined') {
+                        globalThis.Serial = {}
+                        globalThis.Serial.prototype = { requestPort: async () => { /* noop */ } }
+                    }
+                    // @ts-expect-error - testing
+                    if (typeof HID === 'undefined') {
+                        globalThis.HID = {}
+                        globalThis.HID.prototype = { requestDevice: async () => { /* noop */ } }
+                    }
+                })
+            }
+        })
+
         // read the built file from disk and do replacements
         const wrapFn = this.build.switch({
             'apple-isolated': () => wrapWebkitScripts,
